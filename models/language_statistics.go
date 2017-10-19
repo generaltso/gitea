4bf4d160124887eca0c99659d0ef3fbf1f3d0222
tso <tso@teknik.io>
2017-10-19 17:15:32 -0400

    Add statistics bar to template (original design, CSS is a bit wonky)

--- models/language_statistics.go
+++ models/language_statistics.go
@@ -6,17 +6,17 @@ import api "code.gitea.io/sdk/gitea"
 func getLanguageStatistics() []*api.LanguageStat {
 	l1 := &api.LanguageStat{
 		Language:   "PHP",
-		Percentage: 0.47,
+		Percentage: 0.47 * 100,
 		Color:      "#ff0000",
 	}
 	l2 := &api.LanguageStat{
 		Language:   "JavaScript",
-		Percentage: 0.23,
+		Percentage: 0.23 * 100,
 		Color:      "#00ff00",
 	}
 	l3 := &api.LanguageStat{
 		Language:   "CSS",
-		Percentage: 0.3,
+		Percentage: 0.3 * 100,
 		Color:      "#0000ff",
 	}
 	return []*api.LanguageStat{l1, l2, l3}
