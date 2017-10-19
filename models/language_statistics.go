fdcd6b8b9ebfb8098ecfa81f92352c20ed58f270
tso <tso@teknik.io>
2017-10-19 17:27:22 -0400

    Proof-of-concept (in terms of design)

--- models/language_statistics.go
+++ models/language_statistics.go
@@ -1,23 +1,45 @@
 package models
 
-// import "github.com/generaltso/linguist"
-import api "code.gitea.io/sdk/gitea"
+import (
+	"sort"
 
-func getLanguageStatistics() []*api.LanguageStat {
+	api "code.gitea.io/sdk/gitea"
+
+	"github.com/generaltso/linguist"
+)
+
+type LanguageStatistics []*api.LanguageStat
+
+func (l LanguageStatistics) Len() int {
+	return len(l)
+}
+
+func (l LanguageStatistics) Less(i, j int) bool {
+	return l[i].Percentage < l[j].Percentage
+}
+
+func (l LanguageStatistics) Swap(i, j int) {
+	l[i], l[j] = l[j], l[i]
+}
+
+func getLanguageStatistics() LanguageStatistics {
 	l1 := &api.LanguageStat{
 		Language:   "PHP",
-		Percentage: 0.47 * 100,
-		Color:      "#ff0000",
+		Percentage: 0.47,
 	}
 	l2 := &api.LanguageStat{
 		Language:   "JavaScript",
-		Percentage: 0.23 * 100,
-		Color:      "#00ff00",
+		Percentage: 0.23,
 	}
 	l3 := &api.LanguageStat{
 		Language:   "CSS",
-		Percentage: 0.3 * 100,
-		Color:      "#0000ff",
+		Percentage: 0.3,
 	}
-	return []*api.LanguageStat{l1, l2, l3}
+	stats := LanguageStatistics([]*api.LanguageStat{l1, l2, l3})
+	for i, l := range stats {
+		stats[i].Percentage *= 100
+		stats[i].Color = linguist.LanguageColor(l.Language)
+	}
+	sort.Sort(sort.Reverse(stats))
+	return stats
 }
